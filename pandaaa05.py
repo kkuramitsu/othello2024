f1bc65b8ad11c8d18318092f8e4cb96f46ba5cf3
aaako-waka <kurohakukoron@icloud.com>
2024-12-16 16:47:58 +0900

    pandaaa05.py

---
+++ pandaaa05.py
@@ -0,0 +1,60 @@
+import random
+
+BLACK = 1
+WHITE = 2
+
+board = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 2, 0, 0],
+    [0, 0, 2, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+]
+
+def can_place_x_y(board, stone, x, y):
+    if board[y][x] != 0:
+        return False
+
+    opponent = 3 - stone
+    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        found_opponent = False
+
+        while 0 <= nx < len(board[0]) and 0 <= ny < len(board) and board[ny][nx] == opponent:
+            nx += dx
+            ny += dy
+            found_opponent = True
+
+        if found_opponent and 0 <= nx < len(board[0]) and 0 <= ny < len(board) and board[ny][nx] == stone:
+            return True
+
+    return False
+
+def can_place(board, stone):
+    for y in range(len(board)):
+        for x in range(len(board[0])):
+            if can_place_x_y(board, stone, x, y):
+                return True
+    return False
+
+def place_stone(board, stone, x, y):
+    if not can_place_x_y(board, stone, x, y):
+        raise ValueError("Invalid move")
+
+    opponent = 3 - stone
+    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    board[y][x] = stone
+
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        stones_to_flip = []
+
+        while 0 <= nx < len(board[0]) and 0 <= ny < len(board) and board[ny][nx] == opponent:
+            stones_to_flip.append((nx, ny))
+            nx += dx
+            ny += dy
+
+        if 0 <= nx < len(boa
